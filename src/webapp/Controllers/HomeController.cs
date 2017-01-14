be2c48acc6b87ab05651083e23670e45f5a10954
duong <duongphuhiep@gmail.com>
2017-01-14 11:25:14 +0100

    add project references

--- src/webapp/Controllers/HomeController.cs
+++ src/webapp/Controllers/HomeController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Dal;
 
 namespace WebApplication.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private static readonly NamesDb _db = new NamesDb();
+
+        public IActionResult Index(string name)
         {
-            return View();
+            if (!string.IsNullOrEmpty(name))
+                _db.Store(name);
+            return Json(_db.GetAll());
         }
 
         public IActionResult About()
